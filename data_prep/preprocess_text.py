355126f77573f52c20e8c8572e74b7da947209f0
vansh visariya <vanshvisariya1985@gmail.com>
2025-09-02 21:50:52 +0530

    completed and made few changes in prepro text

--- data_prep/preprocess_text.py
+++ data_prep/preprocess_text.py
@@ -1,11 +1,22 @@
+# flow of this preprocessing text is :
+# 1. load text from the file
+# 2. clean text using regex, remove all the symbols and keep only the words
+# 3. prepare dataloader for the input which we will pass to model for training
+#         a. create dataset
+#         b. create dataloader 
+# 4. embed the input_ids using embedding layer and add position embeddings to it
+
 import re
 import tiktoken
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+from config import *
+
+# define how your data is stored and how it's accessed
 class GPTDataset(Dataset):
-    def __init__(self, txt, tokenizer, max_length, stride):
+    def __init__(self, txt, tokenizer):
         self.input_ids = []
         self.target_ids = []
 
@@ -13,9 +24,9 @@ class GPTDataset(Dataset):
         token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
 
         # Use a sliding window to chunk the book into overlapping sequences of max_length
-        for i in range(0, len(token_ids) - max_length, stride):
-            input_chunk = token_ids[i:i + max_length]
-            target_chunk = token_ids[i + 1: i + max_length + 1]
+        for i in range(0, len(token_ids) - MAX_LENGTH, STRIDE):
+            input_chunk = token_ids[i:i + MAX_LENGTH]
+            target_chunk = token_ids[i + 1: i + MAX_LENGTH + 1]
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
         # input = [0,1,2,3]
@@ -32,8 +43,8 @@ class PreprocessText:
         self.file_path = file_path
         self.text = ""
     
-    def load_text(self, file_path):
-        with open(file_path, 'r', encoding='utf-8') as file:
+    def load_text(self):
+        with open(self.file_path, 'r', encoding='utf-8') as file:
             self.text = file.read()
         
     def clean_text(self):
@@ -42,24 +53,43 @@ class PreprocessText:
         result = [item.strip() for item in result if item.strip()]
         self.text = " ".join(result)
     
-    
-    def create_dataloader(self, batch_size=4, max_length=256, 
-                         stride=128 , shuffle=True, drop_last=True,
-                         num_workers=0):
-
+    # Provides batching, shuffling, and parallel loading of data from a Dataset
+    def create_dataloader(self, shuffle=True, drop_last=True, num_workers=0):
         # Initialize the tokenizer
         tokenizer = tiktoken.get_encoding("gpt2")
 
         # Create dataset
-        dataset = GPTDataset(self.text, tokenizer, max_length, stride)
+        dataset = GPTDataset(self.text, tokenizer)
 
         # Create dataloader
         dataloader = DataLoader(
             dataset,
-            batch_size=batch_size,
+            batch_size=BATCH_SIZE,
             shuffle=shuffle,
             drop_last=drop_last,
             num_workers=num_workers
         )
 
         return dataloader
+    
+    def embedding(self, dataloader):
+        embedding_layer = torch.nn.Embedding(VOCAB_SIZE, OUTPUT_DIM)
+        all_embedded_inputs = []
+
+        # Iterate over the dataloader and embed the input_ids
+        for input_ids, _ in dataloader:
+            embedded_input = embedding_layer(input_ids)
+            all_embedded_inputs.append(embedded_input)
+        
+        ## Here i can also use different type of positional embedding like sinusoidal embedding and rotary embedding
+        # This is learned positional embedding
+        pos_embedding_layer = torch.nn.Embedding(MAX_LENGTH, OUTPUT_DIM)
+
+        # Get the position embeddings
+        pos_embeddings = pos_embedding_layer(torch.arange(MAX_LENGTH))
+        # Add the position embeddings to all the input embeddings
+        input_embeddings = all_embedded_inputs + pos_embeddings
+
+        # Concatenate all the embedded inputs and dimension keeps to 0 so that it doesn't change the batch size
+        # return size (num_batches × batch_size, max_length, output_dim)
+        return input_embeddings
